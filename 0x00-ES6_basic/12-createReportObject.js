086ed3e36642fab3f9d4382508f463fb180d4f8b
Moses <mosesmkhonza@outlook.com>
2024-06-24 10:33:15 -0700

    Create report objects

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeesList },
-    getNumberOfDepartments() {
-      return Object.keys
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
