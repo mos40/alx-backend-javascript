ecb13964a97f094355d321214e495fc76a1962a2
Moses <mosesmkhonza@outlook.com>
2024-06-24 09:32:08 -0700

    block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;  // Use let instead of var
-  let task2 = true;  // Use let instead of var
+export default function taskBlock(trueOrFalse)
+{
+	let task = false;  // Use let instead of var
+	let task2 = true;  // Use let instead of var
 
-  if (trueOrFalse) {
-	const task = true;  // Assign to the outer scoped variable task
-	const task2 = false;  // Assign to the outer scoped variable task2
-  }
+	if (trueOrFalse)
+	{
+		const task = true;  // Assign to the outer scoped variable task
+		const task2 = false;  // Assign to the outer scoped variable task2
+	}
 
-  return [task, task2];
+	return [task, task2];
 }
