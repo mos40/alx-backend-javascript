10dabce6c4b29d21bbfef0bc1cce7a1a78fe9db1
Moses <mosesmkhonza@outlook.com>
2024-07-05 12:15:14 +0000

    Students by loc

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Moses Mkhonza <https://github.com/ Mos40>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
